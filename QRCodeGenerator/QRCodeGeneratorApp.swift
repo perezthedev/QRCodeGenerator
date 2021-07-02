1d9019f91252504237bfa9a990b42b99d3e16574
Ryan Perez <ryancs115@gmail.com>
2021-07-01 20:29:12 -0700

    Initial Commit

---
+++ QRCodeGenerator/QRCodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGeneratorApp.swift
+//  QRCodeGenerator
+//
+//  Created by thepercussivedev on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
