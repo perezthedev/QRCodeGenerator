d1f3486eb431e98346c2c43eaa014786b97e9ac6
Ryan Perez <ryancs115@gmail.com>
2021-07-03 20:06:56 -0700

    removed previous SwiftUI files to begin QR Code Generator work

--- QRCodeGenerator/ContentView.swift
+++ QRCodeGenerator/ContentView.swift
@@ -9,36 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Welcome")
-                    .font(.subheadline)
-                HStack {
-                    Text("RCB Tech!")
-                        .font(.title)
-                    Spacer()
-                    Text("Nevada")
-                        .font(.subheadline)
-                    
-                }
-                Divider()
-                Text("About information here")
-                    .font(.title2)
-                
-                Text("Descriptor goes here.")
-            }
-            .padding()
-            Spacer()
-        }
-      
+        Text("Hello")
+        .padding()
     }
 }
 
