df50d9e1d4164001efd2ded87413149072869cfc
Ryan Perez <ryancs115@gmail.com>
2021-07-08 06:18:48 -0700

    changed background color for ContentView

--- QRCodeGenerator/ContentView.swift
+++ QRCodeGenerator/ContentView.swift
@@ -13,41 +13,46 @@ struct ContentView: View {
     @State var message: String = ""
     var body: some View {
         NavigationView {
-            VStack {
-                HStack(alignment: .center){
-                    Text("URL Link:")
-                        .bold()
-                    TextField("Enter your website", text: $website)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .foregroundColor(Color.black)
-                }.padding().shadow(radius: 1)
-                
-                HStack(alignment: .center){
-                    Text("Name:")
-                        .bold()
-                    TextField("Enter your name (optional)", text: $message)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
+            // I want color 236, 236, 236
+            ZStack{
+               // Color(#colorLiteral(red: 0.9198423028, green: 0.9198423028, blue: 0.9198423028, alpha: 1))
+                Color(UIColor.secondarySystemBackground)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    HStack(alignment: .center){
+                        Text("URL Link:")
+                            .bold()
+                        TextField("Enter your website", text: $website)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .foregroundColor(Color.black)
+                    }.padding().shadow(radius: 1)
                     
-                }.padding().shadow(radius: 1)
-                
-                HStack(alignment: .center){
-                    Text("Message:")
-                        .bold()
-                    TextField("Enter your message (optional)", text: $message)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    HStack(alignment: .center){
+                        Text("Name:")
+                            .bold()
+                        TextField("Enter your name (optional)", text: $message)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                    }.padding().shadow(radius: 1)
                     
-                }.padding().shadow(radius: 1)
-                
-                HStack(alignment: .center){
-                    Text("Contact Info:")
-                        .bold()
-                    TextField("Enter contact info (optional)", text: $message)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    HStack(alignment: .center){
+                        Text("Message:")
+                            .bold()
+                        TextField("Enter your message (optional)", text: $message)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                    }.padding().shadow(radius: 1)
                     
-                }.padding().shadow(radius: 1)
-                
-                // creates QR Code button, leading to QR View
-                NavigationLink(destination: QRCodeView(url: "www.devnations.com"), label: {
+                    HStack(alignment: .center){
+                        Text("Contact Info:")
+                            .bold()
+                        TextField("Enter contact info (optional)", text: $message)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                    }.padding().shadow(radius: 1)
+                    
+                    // creates QR Code button, leading to QR View
+                    NavigationLink(destination: QRCodeView(url: "www.devnations.com"), label: {
                         Text("Create QR Code")
                             .bold()
                             .frame(width: 280, height: 50)
@@ -55,11 +60,10 @@ struct ContentView: View {
                             .foregroundColor(.white)
                             .cornerRadius(50)
                     })
+                }
+                .navigationBarTitle("QR Code Generator", displayMode: .large)
+                .offset(y:-70)
             }
-            .navigationBarTitle("QR Code Generator", displayMode: .large)
-            .offset(y:-70)
-            
-           
         }
     }
 }
