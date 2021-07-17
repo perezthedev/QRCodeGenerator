daf1ed107cc4752089d30399964e75bbdcbe19dd
Ryan Perez <ryancs115@gmail.com>
2021-07-16 22:22:05 -0700

    added default string values

--- QRCodeGenerator/Model/QRCode.swift
+++ QRCodeGenerator/Model/QRCode.swift
@@ -2,14 +2,14 @@
 //  QRCode.swift
 //  QRCodeGenerator
 //
-//  Created by thepercussivedev on 7/10/21.
+//  Created by PerezTheDev on 7/10/21.
 //
 
 import Combine
 
 final class QRCode:ObservableObject{
     @Published var website = "www.devnations.com"
-    @Published var name:String = ""
-    @Published var message:String = ""
-    @Published var contactInfo:String = ""
+    @Published var name:String = "PerezTheDev"
+    @Published var message:String = "This is a message!"
+    @Published var contactInfo:String = "(123)456-7890"
 }
